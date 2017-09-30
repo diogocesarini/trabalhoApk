4c9b589b713594b0c048df31a78c442bc2e32ea7
Diogo <diogocesarini@hotmail.com>
2017-09-29 22:18:07 -0300

    primeiro commit

---
+++ app/src/main/java/app/com/revisao/Produto.java
@@ -0,0 +1,42 @@
+package app.com.revisao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Produto {
+
+    private String nome;
+    private String quantidade;
+    private String categoria;
+
+    public Produto(String nome, String quantidade, String categoria) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
